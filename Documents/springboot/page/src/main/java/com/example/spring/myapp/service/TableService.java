804db68b784aded3fedd973d0da4da241cf4df59
3002291 <112863906+3002291@users.noreply.github.com>
2022-11-03 16:33:43 +0900

    Delete Documents directory

--- Documents/springboot/page/src/main/java/com/example/spring/myapp/service/TableService.java
+++
@@ -1,8 +0,0 @@
-package com.example.spring.myapp.service;
-
-import java.util.List;
-import java.util.Map;
-
-public interface TableService {
-	public List<Map<String,Object>> SelectAllList() throws Exception;
-}
